6775ac0f2b8a373652bc6c1ed5b9a63d612830d1
Opilane TTHK <opilane@tthk.ee>
2025-12-05 13:33:39 +0200

    ühe arvu teise andmetüüpi castimine

---
+++ Casting/Casting/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Casting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int arv = 69420;
+            string arvTektsina = arv.ToString();
+            Console.WriteLine(arvTektsina);
+            Console.WriteLine(arv);
+
+            double arvdouble = 6969696969.420420420420d;
+            int arvdoublearvint = 0;
+
+            arvdoublearvint = (int)arvdouble;
+
+            double uusdouble = arv;
+
+            string pikkus = "";
+            Console.WriteLine("Tere Palun sisesta oma pikkus");
+            pikkus = Console.ReadLine();
+            float pikkusFm = float.Parse(pikkus);
+
+            int pikkusCm = (int)pikkusFm;
+            Console.WriteLine($"Sinu pikkus CMides on {pikkusCm}");
+        }
+    }
+}
