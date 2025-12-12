fc3e73aeca1a6fe05fe973319262f66b7f710d1f
Opilane TTHK <opilane@tthk.ee>
2025-12-12 15:39:02 +0200

    Kodutöö kirjeldus

---
+++ Ülesanded/Veeoidla tase/Meetodikodutöö.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Veeoidla_tase
+{
+    public class Meetodikodutöö
+    {
+        public Meetodikodutöö ()
+        {
+            // 1. ülesanne
+            // Kirjuta meetod mis tervitab kasutajat
+            // Kirjuta meetod mis ütleb talle head aega
+            // Meetod kasutatakse programmis, mis apelsin ananass, kus kõigepealt tsüklis küsitakse kasutajalt tema nime
+            // nimi salvestatalse muutujas, muutuja antakse kaasa meetodile, meetodid ise midagi ei tagasta.
+            
+            // 2. ülesannse
+            // kirjuta meetod, miis joonitab käsureale ruudu, võtab parameetriks kaks täisarvu x ja y ja konsolivärvi
+            // kirjuta teine meetod, mis vahetab konsoolivärvi, ja võtab parameetriks kasutaja sisestuse
+            // meetodeid kasutatakse programmib, kus, abrakadabra, küsitakse kasutajalt tema lemmikvärvi, teise meetodi abil
+            // teise meetodi tulemus hoitakse muutujas, ning see antakse kaasa esimesele meetodile, mis joonistab ruudu aga õige värviga
+
+            // varasemate ülesannete osi, saab siin taaskasutada
+
+
+
+        }
+    }
+}
